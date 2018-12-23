70c83558e47a58ae7785da3feea5845334f63afb
acer <acer@192.168.0.104>
2018-12-24 00:40:23 +0530

    two threads t1 and t2, should print multiple of 3 and 5 such that t1 prints multiple of 3 and t2 prints multiple of 5 in case a number is multiple of both 3 and 5 its the t1 which shoucl print such number. Pattern t1-3,t2-5,t1-6,t1-9,t2-10,t1-12,t1-15 and so on

--- MultipleOf3and5/src/com/multipleof3and5/Program.java
+++ MultipleOf3and5/src/com/multipleof3and5/Program.java
@@ -1,5 +1,68 @@
 package com.multipleof3and5;
 
 public class Program {
+	private int counter = 3;
+	private Thread t1;
+	private Thread t2;
+	private final Object synchronizer = new Object();
+	private void print(String msg) throws InterruptedException {
+		System.out.println(msg);
+		Thread.sleep(500);
+	}
+	public void start() {
+		t1 = new Thread() {
+			@Override
+			public void run() {
+				try {
+					synchronized (synchronizer) {
+						while(!isInterrupted()) {
+							if(counter % 3 == 0) {
+								print("1-"+counter);
+								counter++;
+							}else if(counter % 5 != 0) {
+								counter++;
+							}
+							// do not increment for multiple of 5
+							synchronizer.notifyAll();
+							synchronizer.wait();
+						}
+					}
+				}catch(InterruptedException e) {
+					
+				}
+			};
+		};
 
+		t2 = new Thread() {
+			@Override
+			public void run() {
+				try {
+					synchronized (synchronizer) {
+						while(!isInterrupted()) {
+							if(counter % 3 != 0 && counter % 5 == 0) {
+								print("2-"+counter);
+								counter++;
+							}else if(counter % 3 != 0) {
+								counter++;
+							}
+							synchronizer.notifyAll();
+							synchronizer.wait();
+						}
+					}
+				}catch (InterruptedException e) {
+
+				}
+			};
+		};
+		t1.start();
+		t2.start();
+	}
+	public void stop() {
+		if(t1 != null) {
+			t1.interrupt();
+		}
+		if(t2 != null) {
+			t2.interrupt();
+		}
+	}
 }
