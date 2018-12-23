fc0cf8e274966d5276991c4c19b740de0c57e485
acer <acer@192.168.0.104>
2018-12-24 00:42:07 +0530

    adding the main class to initialize

---
+++ MultipleOf3and5/src/com/multipleof3and5/Main.java
@@ -0,0 +1,14 @@
+package com.multipleof3and5;
+
+public class Main {
+	public static void main(String[] args) {
+		Program program = new Program();
+		program.start();
+		try {
+			Thread.sleep(10000);
+		}catch(InterruptedException e) {
+			
+		}
+		program.stop();
+	}
+}
