a17f5788a929328ecfd14c6635b4371824a5c184
nits.krishna <nits.krishna@gmail.com>
2020-05-10 14:21:16 +0530

    Queue using Stack , supports LIFO, concurrent enqueue and dequeue

---
+++ ThreadSynchronizedBlocks/src/synchronization/blocks/Queue.java
@@ -0,0 +1,47 @@
+package synchronization.blocks;
+
+import java.util.Stack;
+
+/**
+ * Queue Implementation with Stack as underlying data-structure and with
+ * parallel (almost) enqueue and dequeue
+ * 
+ * @author krishna_k
+ *
+ */
+public class Queue {
+	
+	private Stack<Integer> addStack = new Stack<>();
+	private Stack<Integer> removeStack = new Stack<>();
+	private final Object addLock = new Object();
+	private final Object removeLock = new Object();
+	
+	public void enqueue(int item) {
+		synchronized (addLock) {
+			addStack.push(item);
+			addLock.notifyAll();
+		}
+	}
+
+	public int dequeue(){
+		int value = 0;
+		synchronized (removeLock) {
+			if (removeStack.isEmpty()) {
+				try {
+					synchronized (addLock) {
+						while (addStack.isEmpty()) {
+							addLock.wait();
+						}
+						while (!addStack.isEmpty()) {
+							removeStack.push(addStack.pop());
+						}
+					}
+				}catch(InterruptedException e) {
+					
+				}
+			}
+			value = removeStack.pop();
+		}
+		return value;
+	}
+}
